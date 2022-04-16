8bbfa85f976634f77049e5acbbc38382041c02c8
jayashree <sjayashree529@gmail.com>
2022-04-16 14:13:57 +0530

    FirstJavascript program

---
+++ selection/LeapYear.js
@@ -0,0 +1,12 @@
+let userInput = require('readline-sync');
+
+let year = userInput.questionInt("Enter Year : ");
+let yearToCheck = parseInt(year);
+let isLeapYear = false;
+
+if ((yearToCheck % 4 == 0 && yearToCheck % 100 == 0) || yearToCheck % 4 == 0) {
+    isLeapYear = true;
+} else {
+    isLeapYear = false;
+}
+console.log(yearToCheck + " is a leap year  " + isLeapYear);
---
+++ sequence/REPL.js
@@ -0,0 +1,9 @@
+console.log(Math.floor(Math.random() * 10 ));
+console.log(Math.floor(Math.random() * 6 + 1));
+
+let number1 = Math.floor(Math.random() * 6 + 1)
+console.log(number1)
+let number2 = Math.floor(Math.random() * 6 + 1)
+console.log(number2)
+let sum = number1 + number2;
+console.log("Sum is : " + sum)
